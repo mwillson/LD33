cfe85170bc40c0373c08d97a3f189d01d5ada83f
Hassan Shaikley <hassan.shaikley@gmail.com>
2015-08-22 19:50:04 -0700

    created a better looking map!

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,17 +5,40 @@ using System.Collections.Generic;       //Allows us to use Lists.
 public class BoardManager : MonoBehaviour {
 
         public GameObject[] groundTiles;          //Array of floor prefabs.
-
+	public GameObject[] wallTiles;
         private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
 
         //private int columns = 10;
-        private int rows = 10;
+        private int rows;
 	
 	List<int[]> data = new List<int[]>(){
-		{	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1},
-		{	1,	1,	1,	0,	0,	0,	0,	0,	0,	0,	1},
-		{	1,	1,	0,	0,	1,	0,	0,	0,	1,	1,	1},
-		{	1,	1,	1,	0,	0,	0,	1,	0,	0,	0,	1}
+		new int[]{	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1, 	1,	1,	1,	1,	1},
+		new int[]{	1,	1,	1,	0,	0,	0,	0,	0,	0,	0, 	0,	0,	0,	1,	1,	1,	1},
+		new int[]{	1,	1,	0,	0,	1,	0,	0,	0,	1,	1,	0,	0,	0,	1,	1,	1,	1},
+		new int[]{	1,	1,	1,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	1,	1,	1},
+		new int[]{	1,	1,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	1,	1,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+
+		new int[]{	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1},
+		new int[]{	1,	1,	0,	0,	1,	0,	0,	0,	1,	1,	0,	0,	0,	1,	1,	1,	1},
+		new int[]{	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1, 	1,	1,	1,	1,	1},
+
+
 	};
 
 		private int columns;
@@ -46,22 +69,29 @@ public class BoardManager : MonoBehaviour {
             //Instantiate Board and set boardHolder to its transform.
             boardHolder = new GameObject ("Board").transform;
 			columns = data[0].Length;
-			rows =  data.Count; //mapArray.Length;
+			rows =  data.Count; 
             //Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
             for(int x = 0; x < columns; x++)
             {
                 //Loop along y axis, starting from -1 to place floor or outerwall tiles.
                 for(int y = 0; y < rows; y++)
                 {
-                    //Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
-                    GameObject toInstantiate = groundTiles[0];
+				GameObject toInstantiate;
+					//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+                    if (data[y][x] == 0){
+						toInstantiate = groundTiles[0];
 
-                    //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
-                    GameObject instance =
-                        Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
 
-                    //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
-                    instance.transform.SetParent (boardHolder);
+					} else { // if ( data[y][x] == 1){
+						toInstantiate = wallTiles[0];
+
+					} 
+				//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+				GameObject instance =
+					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+				
+				//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+				instance.transform.SetParent (boardHolder);
                 }
             }
 
